63a97e64c1abde745d6fc063a36c0549ef6abe1a
Nelson Pato Jimenez <developer.jimenez@gmail.com>
2014-12-06 10:00:44 -0300

    Set the routes constraints for the api

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   #  devise_for :users
-
+  namespace :api, defaults: { format: :json },
+                              constraints: { subdomain: 'api'}, path: '/' do
+  end
 end
