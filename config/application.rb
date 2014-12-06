66df0ab0995a8b04825301341e7a906e3dd9bcbd
Nelson Pato Jimenez <developer.jimenez@gmail.com>
2014-12-06 00:42:09 -0300

    Setting up rspec 3 configuration for the app

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,17 @@ module BananaCash
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true, #specifies to generate a fixture for each model (using a FactoryGirl factory, instead of an actual fixture)
+        view_specs: false, # false says to skip generating view specs. I won’t cover them in this book; instead we’ll use feature specs to test interface elements.
+        helper_specs: false, # skips generating specs for the helper files Rails generates with each controller. As your comfort level with RSpec improves, consider changing this option to true and testing these files.
+        routing_specs: false, # omits a spec file for your config/routes.rb file. If your application is simple, as the one in this book will be, you’re probably safe skipping these specs. As your application grows, however, and takes on more complex routing, it’s a good idea to incorporate routing specs.
+        controller_specs: true,
+        request_specs: false # false skips RSpec’s defaults for adding integration-level specs in spec/requests. We’ll cover this in chapter 8, at which time we’ll just create our own files.
+      g.fixture_replacement :factory_girl, dir: "spec/factories" # tells Rails to generate factories instead of fixtures, and to save them in the spec/factories directory.
+
+    end
   end
 end
