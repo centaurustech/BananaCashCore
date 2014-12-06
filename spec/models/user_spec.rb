c91479ada3461dc226929742a4a4cc461aa647e7
Nelson Pato Jimenez <developer.jimenez@gmail.com>
2014-12-06 15:54:10 -0300

    Adds spec for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
-RSpec.describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+	before { @user = FactoryGirl.build(:user) }
+	subject { @user }
+	# it { should validate_presence_of(:email) }
+	# it { should validate_uniqueness_of(:email) }
+	# it { should validate_confirmation_of(:password) }
+
+	it { should respond_to(:email) }
+	it { should respond_to(:password) }
+	it { should respond_to(:password_confirmation) }
+
+	it { should be_valid }
+
+	describe "when email is not present" do
+		before { @user.email = " " }
+		it { should_not be_valid }
+	end
 end
