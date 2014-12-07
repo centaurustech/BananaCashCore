03b5b497fdd2b6bba8d16bc73f3be67019c61b10
Nelson Pato Jimenez <developer.jimenez@gmail.com>
2014-12-07 01:12:01 -0300

    Adds the user create endpoint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -5,6 +5,15 @@ class Api::V1::UsersController < ApplicationController
 		respond_with User.find(params[:id])
 	end
 
+  def create
+    user = User.new(user_params)
+    if user.save
+      render json: user, status: 201, location: [:api, user]
+    else
+      render json: { errors: user.errors }, status: 422
+    end
+  end
+
   private
 
   def user_params
